bc76d6afb600e52752cc258609d83573dd798971
vikhyathkiran23 <72803191+vikhyathkiran23@users.noreply.github.com>
2024-03-21 18:46:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,272 @@
+score=0;
+function Question_1(e, id){
+    if(confirm("Are You Sure About your answer for Question 1?")==true){
+    if(e==true){
+        score+=1;             
+    }
+    console.log(score)
+    document.getElementById('q1-1').disabled=true;
+    document.getElementById('q1-2').disabled=true;
+    document.getElementById('q1-3').disabled=true;
+    document.getElementById('q1-4').disabled=true;
+    document.getElementById('q1-1').style.backgroundColor='gray'
+    document.getElementById('q1-2').style.backgroundColor='gray'
+    document.getElementById('q1-3').style.backgroundColor='gray'
+    document.getElementById('q1-4').style.backgroundColor='gray'
+    document.getElementById('q2').scrollIntoView();  
+}
+}
+function Question_2(e, id){
+    if(confirm("Are You Sure About your answer for Question 2?")==true){
+    if(e==true){
+        score+=1;             
+    }
+    console.log(score)
+    document.getElementById('q2-1').disabled=true;
+    document.getElementById('q2-2').disabled=true;
+    document.getElementById('q2-3').disabled=true;
+    document.getElementById('q2-4').disabled=true;
+    document.getElementById('q2-1').style.backgroundColor='gray'
+    document.getElementById('q2-2').style.backgroundColor='gray'
+    document.getElementById('q2-3').style.backgroundColor='gray'
+    document.getElementById('q2-4').style.backgroundColor='gray'
+    document.getElementById('q3').scrollIntoView();  
+}
+}
+function Question_3(e, id){
+    if(confirm("Are You Sure About your answer for Question 3?")==true){
+    if(e==true){
+        score+=1;             
+    }
+    console.log(score)
+    document.getElementById('q3-1').disabled=true;
+    document.getElementById('q3-2').disabled=true;
+    document.getElementById('q3-3').disabled=true;
+    document.getElementById('q3-4').disabled=true;
+    document.getElementById('q3-1').style.backgroundColor='gray'
+    document.getElementById('q3-2').style.backgroundColor='gray'
+    document.getElementById('q3-3').style.backgroundColor='gray'
+    document.getElementById('q3-4').style.backgroundColor='gray'
+    document.getElementById('q4').scrollIntoView();  
+}
+}
+function Question_4(e, id){
+    if(confirm("Are You Sure About your answer for Question 4?")==true){
+    if(e==true){
+        score+=1;             
+    }
+    console.log(score)
+    document.getElementById('q4-1').disabled=true;
+    document.getElementById('q4-2').disabled=true;
+    document.getElementById('q4-3').disabled=true;
+    document.getElementById('q4-4').disabled=true;
+    document.getElementById('q4-1').style.backgroundColor='gray'
+    document.getElementById('q4-2').style.backgroundColor='gray'
+    document.getElementById('q4-3').style.backgroundColor='gray'
+    document.getElementById('q4-4').style.backgroundColor='gray'
+    document.getElementById('q5').scrollIntoView();  
+}
+}
+function Question_5(e, id){
+    if(confirm("Are You Sure About your answer for Question 5?")==true){
+    if(e==true){
+        score+=1;             
+    }
+    console.log(score)
+    document.getElementById('q5-1').disabled=true;
+    document.getElementById('q5-2').disabled=true;
+    document.getElementById('q5-3').disabled=true;
+    document.getElementById('q5-4').disabled=true;
+    document.getElementById('q5-1').style.backgroundColor='gray'
+    document.getElementById('q5-2').style.backgroundColor='gray'
+    document.getElementById('q5-3').style.backgroundColor='gray'
+    document.getElementById('q5-4').style.backgroundColor='gray'
+    document.getElementById('q6').scrollIntoView();  
+}
+}
+function Question_6(e, id){
+    if(confirm("Are You Sure About your answer for Question 6?")==true){
+    if(e==true){
+        score+=1;             
+    }
+    console.log(score)
+    document.getElementById('q6-1').disabled=true;
+    document.getElementById('q6-2').disabled=true;
+    document.getElementById('q6-3').disabled=true;
+    document.getElementById('q6-4').disabled=true;
+    document.getElementById('q6-1').style.backgroundColor='gray'
+    document.getElementById('q6-2').style.backgroundColor='gray'
+    document.getElementById('q6-3').style.backgroundColor='gray'
+    document.getElementById('q6-4').style.backgroundColor='gray'
+    document.getElementById('q7').scrollIntoView();  
+}
+}
+function Question_7(e, id){
+    if(confirm("Are You Sure About your answer for Question 7?")==true){
+    if(e==true){
+        score+=1;             
+    }
+    console.log(score)
+    document.getElementById('q7-1').disabled=true;
+    document.getElementById('q7-2').disabled=true;
+    document.getElementById('q7-3').disabled=true;
+    document.getElementById('q7-4').disabled=true;
+    document.getElementById('q7-1').style.backgroundColor='gray'
+    document.getElementById('q7-2').style.backgroundColor='gray'
+    document.getElementById('q7-3').style.backgroundColor='gray'
+    document.getElementById('q7-4').style.backgroundColor='gray'
+    document.getElementById('q8').scrollIntoView();  
+}
+}
+function Question_8(e, id){
+    if(confirm("Are You Sure About your answer for Question 8?")==true){
+    if(e==true){
+        score+=1;             
+    }
+    console.log(score)
+    document.getElementById('q8-1').disabled=true;
+    document.getElementById('q8-2').disabled=true;
+    document.getElementById('q8-3').disabled=true;
+    document.getElementById('q8-4').disabled=true;
+    document.getElementById('q8-1').style.backgroundColor='gray'
+    document.getElementById('q8-2').style.backgroundColor='gray'
+    document.getElementById('q8-3').style.backgroundColor='gray'
+    document.getElementById('q8-4').style.backgroundColor='gray'
+    document.getElementById('q9').scrollIntoView();  
+}
+}
+function Question_9(e, id){
+    if(confirm("Are You Sure About your answer for Question 9?")==true){
+    if(e==true){
+        score+=1;             
+    }
+    console.log(score)
+    document.getElementById('q9-1').disabled=true;
+    document.getElementById('q9-2').disabled=true;
+    document.getElementById('q9-3').disabled=true;
+    document.getElementById('q9-4').disabled=true;
+    document.getElementById('q9-1').style.backgroundColor='gray'
+    document.getElementById('q9-2').style.backgroundColor='gray'
+    document.getElementById('q9-3').style.backgroundColor='gray'
+    document.getElementById('q9-4').style.backgroundColor='gray'
+    document.getElementById('q10').scrollIntoView();  
+}
+}
+function Question_10(e, id){
+    if(confirm("Are You Sure About your answer for Question 10?")==true){
+    if(e==true){
+        score+=1;             
+    }
+    console.log(score)
+    document.getElementById('q10-1').disabled=true;
+    document.getElementById('q10-2').disabled=true;
+    document.getElementById('q10-3').disabled=true;
+    document.getElementById('q10-4').disabled=true;
+    document.getElementById('q10-1').style.backgroundColor='gray'
+    document.getElementById('q10-2').style.backgroundColor='gray'
+    document.getElementById('q10-3').style.backgroundColor='gray'
+    document.getElementById('q10-4').style.backgroundColor='gray'
+    document.getElementById('submit').scrollIntoView();  
+}
+}
+function Submit(){
+    document.getElementById('q1-1').disabled=true;
+    document.getElementById('q1-2').disabled=true;
+    document.getElementById('q1-3').disabled=true;
+    document.getElementById('q1-4').disabled=true;
+    document.getElementById('q1-1').style.backgroundColor='gray'
+    document.getElementById('q1-2').style.backgroundColor='gray'
+    document.getElementById('q1-3').style.backgroundColor='gray'
+    document.getElementById('q1-4').style.backgroundColor='gray'
+
+    document.getElementById('q2-1').disabled=true;
+    document.getElementById('q2-2').disabled=true;
+    document.getElementById('q2-3').disabled=true;
+    document.getElementById('q2-4').disabled=true;
+    document.getElementById('q2-1').style.backgroundColor='gray'
+    document.getElementById('q2-2').style.backgroundColor='gray'
+    document.getElementById('q2-3').style.backgroundColor='gray'
+    document.getElementById('q2-4').style.backgroundColor='gray'
+
+    document.getElementById('q3-1').disabled=true;
+    document.getElementById('q3-2').disabled=true;
+    document.getElementById('q3-3').disabled=true;
+    document.getElementById('q3-4').disabled=true;
+    document.getElementById('q3-1').style.backgroundColor='gray'
+    document.getElementById('q3-2').style.backgroundColor='gray'
+    document.getElementById('q3-3').style.backgroundColor='gray'
+    document.getElementById('q3-4').style.backgroundColor='gray'
+
+    document.getElementById('q4-1').disabled=true;
+    document.getElementById('q4-2').disabled=true;
+    document.getElementById('q4-3').disabled=true;
+    document.getElementById('q4-4').disabled=true;
+    document.getElementById('q4-1').style.backgroundColor='gray'
+    document.getElementById('q4-2').style.backgroundColor='gray'
+    document.getElementById('q4-3').style.backgroundColor='gray'
+    document.getElementById('q4-4').style.backgroundColor='gray'
+
+
+    document.getElementById('q5-1').disabled=true;
+    document.getElementById('q5-2').disabled=true;
+    document.getElementById('q5-3').disabled=true;
+    document.getElementById('q5-4').disabled=true;
+    document.getElementById('q5-1').style.backgroundColor='gray'
+    document.getElementById('q5-2').style.backgroundColor='gray'
+    document.getElementById('q5-3').style.backgroundColor='gray'
+    document.getElementById('q5-4').style.backgroundColor='gray'
+
+    document.getElementById('q6-1').disabled=true;
+    document.getElementById('q6-2').disabled=true;
+    document.getElementById('q6-3').disabled=true;
+    document.getElementById('q6-4').disabled=true;
+    document.getElementById('q6-1').style.backgroundColor='gray'
+    document.getElementById('q6-2').style.backgroundColor='gray'
+    document.getElementById('q6-3').style.backgroundColor='gray'
+    document.getElementById('q6-4').style.backgroundColor='gray'
+
+    document.getElementById('q7-1').disabled=true;
+    document.getElementById('q7-2').disabled=true;
+    document.getElementById('q7-3').disabled=true;
+    document.getElementById('q7-4').disabled=true;
+    document.getElementById('q7-1').style.backgroundColor='gray'
+    document.getElementById('q7-2').style.backgroundColor='gray'
+    document.getElementById('q7-3').style.backgroundColor='gray'
+    document.getElementById('q7-4').style.backgroundColor='gray'
+    
+    document.getElementById('q8-1').disabled=true;
+    document.getElementById('q8-2').disabled=true;
+    document.getElementById('q8-3').disabled=true;
+    document.getElementById('q8-4').disabled=true;
+    document.getElementById('q8-1').style.backgroundColor='gray'
+    document.getElementById('q8-2').style.backgroundColor='gray'
+    document.getElementById('q8-3').style.backgroundColor='gray'
+    document.getElementById('q8-4').style.backgroundColor='gray'
+
+    document.getElementById('q9-1').disabled=true;
+    document.getElementById('q9-2').disabled=true;
+    document.getElementById('q9-3').disabled=true;
+    document.getElementById('q9-4').disabled=true;
+    document.getElementById('q9-1').style.backgroundColor='gray'
+    document.getElementById('q9-2').style.backgroundColor='gray'
+    document.getElementById('q9-3').style.backgroundColor='gray'
+    document.getElementById('q9-4').style.backgroundColor='gray'
+
+    document.getElementById('q10-1').disabled=true;
+    document.getElementById('q10-2').disabled=true;
+    document.getElementById('q10-3').disabled=true;
+    document.getElementById('q10-4').disabled=true;
+    document.getElementById('q10-1').style.backgroundColor='gray'
+    document.getElementById('q10-2').style.backgroundColor='gray'
+    document.getElementById('q10-3').style.backgroundColor='gray'
+    document.getElementById('q10-4').style.backgroundColor='gray'
+
+
+
+    document.getElementById("Score").style.visibility="visible"
+    document.getElementById("Score").style.width="100%"
+    document.getElementById("Score").style.textAlign="center"
+    document.getElementById("Score").innerHTML="Your score is " + score +" Out of 10"
+    document.getElementById("Score").scrollIntoView();
+    console.log(`${score} is the score`)
+}
